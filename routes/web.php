6bd5bb5ea8f030dafaa8a409a67a11abf9285332
mmdaranjmob <mmdaranjmob@gmail.com>
2026-09-19 21:04:55 +0330

    Set Vertex landing page as homepage

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ use App\Http\Controllers\StoreController;
 use App\Http\Controllers\TicketController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [StoreController::class, 'home'])->name('home');
+Route::get('/', fn () => response()->file(public_path('vertex.html')))->name('home');
 Route::get('/products/{product}', [StoreController::class, 'product'])->name('product.show');
 
 Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
