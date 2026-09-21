a82109765f7abdbf1fec0ecf3637cc128bb77670
mmdaranjmob <mmdaranjmob@gmail.com>
2026-09-21 18:36:24 +0330

    fix: support Fara product card variants

--- resources/views/store/partials/fara-product-card.blade.php
+++ resources/views/store/partials/fara-product-card.blade.php
@@ -1,8 +1,8 @@
 @php
     $currency = $product->currency === 'IRR' ? 'تومان' : ($product->currency ?: 'تومان');
-    $hasDiscount = ($product->original_price ?? null) && $product->original_price > $product->price;
+    $hasDiscount = ($product->original_price ?? null) && $product->original_price > $product->price;\n    $promo = $promo ?? false;\n    $compact = $compact ?? false;
 @endphp
-<div class="styles__product___tP7kV {{ $hasDiscount ? 'styles__two-columns___X1oD5' : '' }}">
+<div class="styles__product___tP7kV grid__span-1___sEgUc {{ $compact ? 'styles__two-columns___X1oD5' : '' }} {{ $promo ? 'styles__product___KsPB7' : '' }}">
     <a class="styles__link___DUbrB general__clear___h0wo9" href="{{ route('product.show',$product) }}">
         @if($hasDiscount)
             <span class="smartTextColor__wrapper___BmwOo smartTextColor__accent___o6C0X smartTextColor__normal____fdpD smartTextColor__background___g52rg styles__discount-tag___nlxNa">تخفیف</span>
